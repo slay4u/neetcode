def4cf3b29a5ad7b70da047f7122087c388c801c
Valentyn Dymura <110052064+slay4u@users.noreply.github.com>
2026-04-21 13:18:11 +0200

    Add: unique-email-addresses - submission-4

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-4.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<>();
+        StringBuilder str = new StringBuilder();
+        for (String s : emails) {
+            int i = 0;
+            while (i < s.length()) {
+                if (s.charAt(i) == '.') {
+                    i++;
+                    continue;
+                }
+                if (s.charAt(i) == '+') {
+                    while (s.charAt(i) != '@') {
+                        i++;
+                        continue;
+                    }
+                    str.append(s.substring(i,s.length()));
+                }
+                //if (s.charAt(i) == '@') str.append(s.substring(i,s.length()));
+                str.append(s.charAt(i));
+                i++;
+            }
+            set.add(str.toString());
+            str.setLength(0);
+        }
+        return set.size();
+    }
+}
