a05006f773ed0343eb212c6a6de18d3823c9e743
Valentyn Dymura <110052064+slay4u@users.noreply.github.com>
2026-05-20 18:22:23 +0200

    Add: is-subsequence - submission-9

---
+++ Data Structures & Algorithms/is-subsequence/submission-9.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int j = 0;
+        for (int i = 0; i < t.length(); i++)
+            if (j < s.length() && s.charAt(j) == t.charAt(i)) j++;
+        return s.length() == j;
+    }
+}
