77d64bce916bb1201e761f536ae34cd9d66e4556
Valentyn Dymura <110052064+slay4u@users.noreply.github.com>
2026-06-23 17:49:32 +0200

    Add: rotate-array - submission-5

---
+++ Data Structures & Algorithms/rotate-array/submission-5.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        rev(nums, 0, nums.length-1);
+        rev(nums, 0, k%nums.length-1);
+        rev(nums, k%nums.length, nums.length-1);
+    }
+
+    void rev(int[] arr, int l, int r) {
+            while (l < r) {
+                int temp = arr[r];
+                arr[r] = arr[l];
+                arr[l++] = temp;
+                //l++;
+                r--;
+            }
+        }
+}
