d2f23adca459abcd1b3d4eec13938993e0fe9d83
Valentyn Dymura <110052064+slay4u@users.noreply.github.com>
2026-07-20 23:21:57 +0200

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : strs) sb.append(s.length()).append("#").append(s);
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            int j = 0;
+            while (Character.isDigit(str.charAt(i))) 
+                j = j * 10 + str.charAt(i++) - '0';
+            j += i += 1;
+            res.add(str.substring(i, j));
+            i = j - 1;
+        }
+        return res;
+    }
+}
