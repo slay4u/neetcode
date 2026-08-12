ac9cf999ce1716557fa7724e0d6242a46dc0c046
Valentyn Dymura <110052064+slay4u@users.noreply.github.com>
2026-08-12 16:06:06 +0200

    Add: string-compression - submission-12

---
+++ Data Structures & Algorithms/string-compression/submission-12.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder s = new StringBuilder();
+        for (int l = 0; l < chars.length;) {
+            s.append(chars[l]);
+            int r = l + 1, count = 1;
+            while (r < chars.length && chars[l] == chars[r]) {
+                r++;
+                count++;
+            }
+            if (count > 1) s.append(count);
+            l = r;
+        }
+        for (int i = 0; i < s.length(); i++) 
+            chars[i] = s.charAt(i);
+        return s.length();
+    }
+}
